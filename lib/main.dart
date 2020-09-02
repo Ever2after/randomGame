94fb991c9da9a95bb61b293bd448d52204b5c909
오주상 <53650934+Everafter20@users.noreply.github.com>
2020-09-02 19:27:32 +0900

    logo update

--- lib/main.dart
+++ lib/main.dart
@@ -23,12 +23,12 @@ final List<String> routes = [   //모든 라우팅 정보는 이곳에
 ];
 
 final List<List<String>> gameInfo = [  //게임 정보 [게임 로고, 이름, 설명, 라우팅 정보]
-  ['images/logo.png', '클레오파트라', '목청이 클수록 유리한 게임', routes[0]],
-  ['images/logo.png', '랜덤초성게임', '', routes[1]],
-  ['images/logo.png', 'Up & Down', '랜덤 숫자 맞추기', routes[2]],
-  ['images/logo.png', '소주병 돌리기', '누군가 한 명 고르고 싶을 때', routes[3]],
-  ['images/logo.png', '주루마블', '', routes[4]],
-  ['images/logo.png', '랜덤터치', '', routes[5]],
+  ['images/logo/3.png', '클레오파트라', '목청이 클수록 유리한 게임', routes[0]],
+  ['images/logo/6.png', '랜덤초성게임', '', routes[1]],
+  ['images/logo/4.png', 'Up & Down', '랜덤 숫자 맞추기', routes[2]],
+  ['images/logo/1.png', '소주병 돌리기', '누군가 한 명 고르고 싶을 때', routes[3]],
+  ['images/logo/5.png', '주루마블', '', routes[4]],
+  ['images/logo/2.png', '랜덤터치', '', routes[5]],git
 ];
 
 class MyApp extends StatelessWidget {
